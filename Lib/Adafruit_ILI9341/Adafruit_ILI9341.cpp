ad87e19cbdfb05a73934f7151b9e5a4076ea70f7
grosseruser <git@elfri.de>
2016-02-28 03:30:50 +0100

    more speed and 32 bit transfer

--- Lib/Adafruit_ILI9341/Adafruit_ILI9341.cpp
+++ Lib/Adafruit_ILI9341/Adafruit_ILI9341.cpp
@@ -14,11 +14,7 @@
  ****************************************************/
 
 #include "Adafruit_ILI9341.h"
-#ifdef __AVR
-  #include <avr/pgmspace.h>
-#elif defined(ESP8266)
-  #include <pgmspace.h>
-#endif
+#include <pgmspace.h>
 #include <limits.h>
 #include "pins_arduino.h"
 #include "wiring_private.h"
@@ -28,34 +24,16 @@
 // If the SPI library has transaction support, these functions
 // establish settings and protect from interference from other
 // libraries.  Otherwise, they simply do nothing.
-#ifdef SPI_HAS_TRANSACTION
 static inline void spi_begin(void) __attribute__((always_inline));
 static inline void spi_begin(void) {
 	// max speed!
 	SPI.beginTransaction(SPISettings(80000000, MSBFIRST, SPI_MODE0));
 	//SPI.setClockDivider(0x00000001);
-	//yield();
+	yield();
 }
 static inline void spi_end(void) __attribute__((always_inline));
 static inline void spi_end(void) {
-  SPI.endTransaction();
-}
-#else
-#define spi_begin()
-#define spi_end()
-#endif
-
-
-// Constructor when using software SPI.  All output pins are configurable.
-Adafruit_ILI9341::Adafruit_ILI9341(int8_t cs, int8_t dc, int8_t mosi,
-				   int8_t sclk, int8_t rst, int8_t miso) : Adafruit_GFX(ILI9341_TFTWIDTH, ILI9341_TFTHEIGHT) {
-  _cs   = cs;
-  _dc   = dc;
-  _mosi  = mosi;
-  _miso = miso;
-  _sclk = sclk;
-  _rst  = rst;
-  hwSPI = false;
+	SPI.endTransaction();
 }
 
 
@@ -69,6 +47,11 @@ Adafruit_ILI9341::Adafruit_ILI9341(int8_t cs, int8_t dc, int8_t rst) : Adafruit_
   _mosi  = _sclk = 0;
 }
 
+void Adafruit_ILI9341::setDataBits(uint16_t bits) {
+    const uint32_t mask = ~((SPIMMOSI << SPILMOSI) | (SPIMMISO << SPILMISO));
+    bits--;
+    SPI1U1 = ((SPI1U1 & mask) | ((bits << SPILMOSI) | (bits << SPILMISO)));
+}
 
 void Adafruit_ILI9341::writecommand(uint8_t c) {
 	digitalWrite(_dc, LOW);
@@ -94,254 +77,112 @@ void Adafruit_ILI9341::writedata16(uint16_t c) {
 	digitalWrite(_cs, HIGH);
 } 
 
+void Adafruit_ILI9341::writedata32(uint32_t c) {
+	digitalWrite(_dc, HIGH);
+	digitalWrite(_sclk, LOW);
+	digitalWrite(_cs, LOW);
+	SPI.write32(c);
+	digitalWrite(_cs, HIGH);
+} 
 
-// Rather than a bazillion writecommand() and writedata() calls, screen
-// initialization commands and arguments are organized in these tables
-// stored in PROGMEM.  The table may look bulky, but that's mostly the
-// formatting -- storage-wise this is hundreds of bytes more compact
-// than the equivalent code.  Companion function follows.
-#define DELAY 0x80
 
-
-// Companion code to the above tables.  Reads and issues
-// a series of LCD commands stored in PROGMEM byte array.
-void Adafruit_ILI9341::commandList(uint8_t *addr) {
-
-  uint8_t  numCommands, numArgs;
-  uint16_t ms;
-
-  numCommands = pgm_read_byte(addr++);   // Number of commands to follow
-  while(numCommands--) {                 // For each command...
-    writecommand(pgm_read_byte(addr++)); //   Read, issue command
-    numArgs  = pgm_read_byte(addr++);    //   Number of args to follow
-    ms       = numArgs & DELAY;          //   If hibit set, delay follows args
-    numArgs &= ~DELAY;                   //   Mask out delay bit
-    while(numArgs--) {                   //   For each argument...
-      writedata(pgm_read_byte(addr++));  //     Read, issue argument
-    }
-
-    if(ms) {
-      ms = pgm_read_byte(addr++); // Read post-command delay time (ms)
-      if(ms == 255) ms = 500;     // If 255, delay for 500 ms
-      delay(ms);
-    }
-  }
-}
 
 
 void Adafruit_ILI9341::begin(void) {
-  if (_rst > 0) {
-    pinMode(_rst, OUTPUT);
-    digitalWrite(_rst, LOW);
-  }
 
-  pinMode(_dc, OUTPUT);
-  pinMode(_cs, OUTPUT);
-
-#if defined(__AVR__)
-  csport    = portOutputRegister(digitalPinToPort(_cs));
-  dcport    = portOutputRegister(digitalPinToPort(_dc));
-#elif defined(ESP8266)
-  mosiport = &_mosi;
-  clkport = &_sclk;
-  rsport = &_rst;
-  csport    = &_cs;
-  dcport    = &_dc;
-#endif
-  
-  cspinmask = digitalPinToBitMask(_cs);
-  dcpinmask = digitalPinToBitMask(_dc);
-
-    SPI.begin();
-
-#ifndef SPI_HAS_TRANSACTION
-    SPI.setBitOrder(MSBFIRST);
-    SPI.setDataMode(SPI_MODE0);
-  #if defined (__AVR__)
-    //SPI.setClockDivider(SPI_CLOCK_DIV2); // 8 MHz (full! speed!)
-    SPI.setFrequency(500000); // 8 MHz (full! speed!)
-    mySPCR = SPCR;
-  #elif defined(TEENSYDUINO) || defined(ESP8266)
-    //SPI.setClockDivider(SPI_CLOCK_DIV2); // 8 MHz (full! speed!)
-    SPI.setFrequency(500000); // 8 MHz (full! speed!)
-  #elif defined (__arm__)
-    SPI.setClockDivider(11); // 8-ish MHz (full! speed!)
-  #endif
-#endif
-/*
-  if(hwSPI) { // Using hardware SPI
-  } else {
-    pinMode(_sclk, OUTPUT);
-    pinMode(_mosi, OUTPUT);
-    pinMode(_miso, INPUT);
-
-#if defined(__AVR__)
-    clkport     = portOutputRegister(digitalPinToPort(_sclk));
-    mosiport    = portOutputRegister(digitalPinToPort(_mosi));
-#endif
+	pinMode(_dc, OUTPUT);
+	pinMode(_cs, OUTPUT);
 	
-    clkpinmask  = digitalPinToBitMask(_sclk);
-    mosipinmask = digitalPinToBitMask(_mosi);
-    *clkport   &= ~clkpinmask;
-    *mosiport  &= ~mosipinmask;
-  }
-*/
-  // toggle RST low to reset
-  if (_rst > 0) {
-    digitalWrite(_rst, HIGH);
-    delay(5);
-    digitalWrite(_rst, LOW);
-    delay(20);
-    digitalWrite(_rst, HIGH);
-    delay(150);
-  }
-
-  /*
-  uint8_t x = readcommand8(ILI9341_RDMODE);
-  Serial.print("\nDisplay Power Mode: 0x"); Serial.println(x, HEX);
-  x = readcommand8(ILI9341_RDMADCTL);
-  Serial.print("\nMADCTL Mode: 0x"); Serial.println(x, HEX);
-  x = readcommand8(ILI9341_RDPIXFMT);
-  Serial.print("\nPixel Format: 0x"); Serial.println(x, HEX);
-  x = readcommand8(ILI9341_RDIMGFMT);
-  Serial.print("\nImage Format: 0x"); Serial.println(x, HEX);
-  x = readcommand8(ILI9341_RDSELFDIAG);
-  Serial.print("\nSelf Diagnostic: 0x"); Serial.println(x, HEX);
-*/
-  //if(cmdList) commandList(cmdList);
+    SPI.begin();
   
-  if (hwSPI) spi_begin();
-  writecommand(0xEF);
-  writedata(0x03);
-  writedata(0x80);
-  writedata(0x02);
+	spi_begin();
+	writecommand(0xEF);
+	writedata16((0x03<<8)|(0x80));
+	writedata(0x02);
 
-  writecommand(0xCF);  
-  writedata(0x00); 
-  writedata(0XC1); 
-  writedata(0X30); 
+	writecommand(0xCF);  
+	writedata16((0x00<<8)|(0XC1)); 
+	writedata(0X30); 
 
-  writecommand(0xED);  
-  writedata(0x64); 
-  writedata(0x03); 
-  writedata(0X12); 
-  writedata(0X81); 
+	writecommand(0xED);  
+	writedata32((0x64<<24)|(0x03<<16)|(0X12<<8)|(0X81)); 
  
-  writecommand(0xE8);  
-  writedata(0x85); 
-  writedata(0x00); 
-  writedata(0x78); 
+	writecommand(0xE8);  
+	writedata16((0x85<<8)|(0x00)); 
+	writedata(0x78); 
 
-  writecommand(0xCB);  
-  writedata(0x39); 
-  writedata(0x2C); 
-  writedata(0x00); 
-  writedata(0x34); 
-  writedata(0x02); 
+	writecommand(0xCB);  
+	writedata32((0x39<<24)|(0x2C<<16)|(0x00<<8)|(0x34)); 
+	writedata(0x02); 
  
-  writecommand(0xF7);  
-  writedata(0x20); 
+	writecommand(0xF7);  
+	writedata(0x20); 
 
-  writecommand(0xEA);  
-  writedata(0x00); 
-  writedata(0x00); 
+	writecommand(0xEA);  
+	writedata16((0x00<<8)|(0x00)); 
  
-  writecommand(ILI9341_PWCTR1);    //Power control 
-  writedata(0x23);   //VRH[5:0] 
+	writecommand(ILI9341_PWCTR1);    //Power control 
+	writedata(0x23);   //VRH[5:0] 
  
-  writecommand(ILI9341_PWCTR2);    //Power control 
-  writedata(0x10);   //SAP[2:0];BT[3:0] 
+	writecommand(ILI9341_PWCTR2);    //Power control 
+	writedata(0x10);   //SAP[2:0];BT[3:0] 
  
-  writecommand(ILI9341_VMCTR1);    //VCM control 
-  writedata(0x3e); //¶Ô±È¶Èµ÷½Ú
-  writedata(0x28); 
+	writecommand(ILI9341_VMCTR1);    //VCM control 
+	writedata16((0x3e<<8)|(0x28)); //¶Ô±È¶Èµ÷½Ú
   
-  writecommand(ILI9341_VMCTR2);    //VCM control2 
-  writedata(0x86);  //--
+	writecommand(ILI9341_VMCTR2);    //VCM control2 
+	writedata(0x86);  //--
  
-  writecommand(ILI9341_MADCTL);    // Memory Access Control 
-  writedata(0x48);
+	writecommand(ILI9341_MADCTL);    // Memory Access Control 
+	writedata(0x48);
 
-  writecommand(ILI9341_PIXFMT);    
-  writedata(0x55); 
+	writecommand(ILI9341_PIXFMT);    
+	writedata(0x55); 
   
-  writecommand(ILI9341_FRMCTR1);    
-  writedata(0x00);  
-  writedata(0x18); 
- 
-  writecommand(ILI9341_DFUNCTR);    // Display Function Control 
-  writedata(0x08); 
-  writedata(0x82);
-  writedata(0x27);  
- 
-  writecommand(0xF2);    // 3Gamma Function Disable 
-  writedata(0x00); 
- 
-  writecommand(ILI9341_GAMMASET);    //Gamma curve selected 
-  writedata(0x01); 
- 
-  writecommand(ILI9341_GMCTRP1);    //Set Gamma 
-  writedata(0x0F); 
-  writedata(0x31); 
-  writedata(0x2B); 
-  writedata(0x0C); 
-  writedata(0x0E); 
-  writedata(0x08); 
-  writedata(0x4E); 
-  writedata(0xF1); 
-  writedata(0x37); 
-  writedata(0x07); 
-  writedata(0x10); 
-  writedata(0x03); 
-  writedata(0x0E); 
-  writedata(0x09); 
-  writedata(0x00); 
-  
-  writecommand(ILI9341_GMCTRN1);    //Set Gamma 
-  writedata(0x00); 
-  writedata(0x0E); 
-  writedata(0x14); 
-  writedata(0x03); 
-  writedata(0x11); 
-  writedata(0x07); 
-  writedata(0x31); 
-  writedata(0xC1); 
-  writedata(0x48); 
-  writedata(0x08); 
-  writedata(0x0F); 
-  writedata(0x0C); 
-  writedata(0x31); 
-  writedata(0x36); 
-  writedata(0x0F); 
+	writecommand(ILI9341_FRMCTR1);    
+	writedata16((0x00<<8)|(0x18));  
+	
+	writecommand(ILI9341_DFUNCTR);    // Display Function Control 
+	writedata16((0x08<<8)|(0x82)); 
+	writedata(0x27);  
+	
+	writecommand(0xF2);    // 3Gamma Function Disable 
+	writedata(0x00); 
+	
+	writecommand(ILI9341_GAMMASET);    //Gamma curve selected 
+	writedata(0x01); 
+	
+	writecommand(ILI9341_GMCTRP1);    //Set Gamma 
+	writedata32((0x0F<<24)|(0x31<<16)|(0x2B<<8)|(0x0C)); 
+	writedata32((0x0E<<24)|(0x08<<16)|(0x4E<<8)|(0xF1)); 
+	writedata32((0x37<<24)|(0x07<<16)|(0x10<<8)|(0x03)); 
+	writedata16((0x0E<<8)|(0x09)); 
+	writedata(0x00); 
+	
+	writecommand(ILI9341_GMCTRN1);    //Set Gamma 
+	writedata32((0x00<<24)|(0x0E<<16)|(0x14<<8)|(0x03)); 
+	writedata32((0x11<<24)|(0x07<<16)|(0x31<<8)|(0xC1)); 
+	writedata32((0x48<<24)|(0x08<<16)|(0x0F<<8)|(0x0C)); 
+	writedata16((0x31<<8)|(0x36)); 
+	writedata(0x0F); 
 
-  writecommand(ILI9341_SLPOUT);    //Exit Sleep 
-  if (hwSPI) spi_end();
-  delay(120); 		
-  if (hwSPI) spi_begin();
-  writecommand(ILI9341_DISPON);    //Display on 
-  if (hwSPI) spi_end();
+	writecommand(ILI9341_SLPOUT);    //Exit Sleep 
+	spi_end();
+	delay(120); 		
+	spi_begin();
+	writecommand(ILI9341_DISPON);    //Display on 
+	spi_end();
 
 }
 
-void Adafruit_ILI9341::setDataBits(uint16_t bits) {
-    const uint32_t mask = ~((SPIMMOSI << SPILMOSI) | (SPIMMISO << SPILMISO));
-    bits--;
-    SPI1U1 = ((SPI1U1 & mask) | ((bits << SPILMOSI) | (bits << SPILMISO)));
-}
 
 void Adafruit_ILI9341::setAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1) {
-
-  writecommand(ILI9341_CASET); // Column addr set
-  writedata16(x0);     // XSTART 
-  writedata16(x1);     // XEND
-
-  writecommand(ILI9341_PASET); // Row addr set
-  writedata16(y0);     // YSTART
-  writedata16(y1);     // YEND
-
-  writecommand(ILI9341_RAMWR); // write to RAM
+	writecommand(ILI9341_CASET); // Column addr set
+	writedata32((x0<<16)|x1);
+	writecommand(ILI9341_PASET); // Row addr set
+	writedata32((y0<<16)|y1);
+	writecommand(ILI9341_RAMWR); // write to RAM
 }
-
 
 void Adafruit_ILI9341::pushColor(uint16_t color) {
 	spi_begin();
@@ -378,13 +219,23 @@ void Adafruit_ILI9341::drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t c
 	digitalWrite(_cs, LOW);
 	
 	while(SPI1CMD & SPIBUSY) {}
-	setDataBits(16);
-	while (h--) {
-		//SPI.write16(color);
+	uint32_t c32 = (color<<16)|color;
+	
+	if(h%2){
+		setDataBits(16);
 		SPI1W0 = color;
 		SPI1CMD |= SPIBUSY;
 		while(SPI1CMD & SPIBUSY) {}
+		h--;
 	}
+	setDataBits(32);
+	while(h){
+		SPI1W0 = c32;
+		SPI1CMD |= SPIBUSY;
+		while(SPI1CMD & SPIBUSY) {}
+		h-=2;
+	}
+
 	
 	digitalWrite(_cs, HIGH);
 	spi_end();
@@ -403,12 +254,21 @@ void Adafruit_ILI9341::drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t c
 	digitalWrite(_cs, LOW);
 	
 	while(SPI1CMD & SPIBUSY) {}
-	setDataBits(16);
-	while (w--) {
-		//SPI.write16(color);
+	uint32_t c32 = (color<<16)|color;
+	
+	if(w%2){
+		setDataBits(16);
 		SPI1W0 = color;
 		SPI1CMD |= SPIBUSY;
 		while(SPI1CMD & SPIBUSY) {}
+		w--;
+	}
+	setDataBits(32);
+	while(w){
+		SPI1W0 = c32;
+		SPI1CMD |= SPIBUSY;
+		while(SPI1CMD & SPIBUSY) {}
+		w-=2;
 	}
 
 	digitalWrite(_cs, HIGH);
@@ -418,7 +278,6 @@ void Adafruit_ILI9341::drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t c
 void Adafruit_ILI9341::fillScreen(uint16_t color) {
 	fillRect(0, 0,  _width, _height, color);
 }
-
 
 // fill a rectangle
 void Adafruit_ILI9341::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color) {
@@ -434,14 +293,22 @@ void Adafruit_ILI9341::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint
 	digitalWrite(_cs, LOW);
 	
 	while(SPI1CMD & SPIBUSY) {}
-	setDataBits(16);
-	for(y=h; y>0; y--) {
-		for(x=w; x>0; x--) {
-			//SPI.write16(color);
-			SPI1W0 = color;
-			SPI1CMD |= SPIBUSY;
-			while(SPI1CMD & SPIBUSY) {}
-		}
+	uint32_t cn = h * w;
+	uint32_t c32 = (color<<16)|color;
+	
+	if(cn%2){
+		setDataBits(16);
+		SPI1W0 = color;
+		SPI1CMD |= SPIBUSY;
+		while(SPI1CMD & SPIBUSY) {}
+		cn--;
+	}
+	setDataBits(32);
+	while(cn){
+		SPI1W0 = c32;
+		SPI1CMD |= SPIBUSY;
+		while(SPI1CMD & SPIBUSY) {}
+		cn-=2;
 	}
 	
 	digitalWrite(_cs, HIGH);
--- Lib/Adafruit_ILI9341/Adafruit_ILI9341.h
+++ Lib/Adafruit_ILI9341/Adafruit_ILI9341.h
@@ -118,32 +118,38 @@ class Adafruit_ILI9341 : public Adafruit_GFX {
   Adafruit_ILI9341(int8_t _CS, int8_t _DC, int8_t _MOSI, int8_t _SCLK, int8_t _RST, int8_t _MISO);
   Adafruit_ILI9341(int8_t _CS, int8_t _DC, int8_t _RST = -1);
 
-  void     begin(void),
-           setAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1),
-           pushColor(uint16_t color),
-           fillScreen(uint16_t color),
-           drawPixel(int16_t x, int16_t y, uint16_t color),
-           drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color),
-           drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color),
-           fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color),
-           setRotation(uint8_t r),
-           invertDisplay(boolean i);
-  uint16_t color565(uint8_t r, uint8_t g, uint8_t b);
+	void     
+		begin(void),
+		setAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1),
+		pushColor(uint16_t color),
+		fillScreen(uint16_t color),
+		drawPixel(int16_t x, int16_t y, uint16_t color),
+		drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color),
+		drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color),
+		fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color),
+		setRotation(uint8_t r),
+		invertDisplay(boolean i);
+	uint16_t 
+		color565(uint8_t r, uint8_t g, uint8_t b);
 
   /* These are not for current use, 8-bit protocol only! */
-  uint8_t  readdata(void),
-    readcommand8(uint8_t reg, uint8_t index = 0);
+	uint8_t  
+		readdata(void),
+		readcommand8(uint8_t reg, uint8_t index = 0);
   /*
   uint16_t readcommand16(uint8_t);
   uint32_t readcommand32(uint8_t);
   void     dummyclock(void);
-  */
+*/
 
-  void     spiwrite(uint8_t),
-    writecommand(uint8_t c),
-    writedata(uint8_t d),
-    commandList(uint8_t *addr);
-  uint8_t  spiread(void);
+	void     
+		writecommand(uint8_t c),
+		writedata(uint8_t d),
+		writedata16(uint16_t d),
+		writedata32(uint32_t d),
+		commandList(uint8_t *addr),
+		setDataBits(uint16_t bits);
+	uint8_t  spiread(void);
 
  private:
   uint8_t  tabcolor;
